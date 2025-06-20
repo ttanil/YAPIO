a4dd192abad15066288c164ba6fc62913ca82696
Tahsin Tanıl Uçar <tahsintanil@gmail.com>
2025-06-21 00:56:13 +0300

    update

--- dbs.js
+++ dbs.js
@@ -7,15 +7,15 @@ const DB_URL = process.env.NODE_ENV === 'production'
   : process.env.DB_URL_LOCAL;
 
 const conn = () => {
-    mongoose.connect(DB_URL, {
-        dbName: 'insaathesap'
-    })
-    .then(() => {
-        console.log("DB connected");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  mongoose.connect(DB_URL, {
+    dbName: 'insaathesap'
+})
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB bağlantı hatası:", err);
+  });
 };
 
 module.exports = conn;
