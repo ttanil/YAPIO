7b4f3cddd1c34e7811011db26608d7f5db20ed1c
Tahsin Tanıl Uçar <tahsintanil@gmail.com>
2025-06-21 00:39:30 +0300

    update

--- dbs.js
+++ dbs.js
@@ -2,16 +2,20 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const DB_URL = process.env.NODE_ENV === 'production'
+  ? process.env.DB_URL_PROD
+  : process.env.DB_URL_LOCAL;
 
-const conn = ()=>{
-    mongoose.connect(process.env.DB_URL,{
-        dbName:'insaathesap'
-    }).then(()=>{
-        console.log("DB connected")
-    }) 
-    .catch((err)=>{
-        console.log(err)
+const conn = () => {
+    mongoose.connect(DB_URL, {
+        dbName: 'insaathesap'
     })
-}
+    .then(() => {
+        console.log("DB connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+};
 
 module.exports = conn;
